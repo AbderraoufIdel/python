a04587fcdcb199ac8723e43dd72cc629565e7a0c
Abderraouf Idel <abderraoufidel@gmail.com>
2024-10-02 17:02:02 +0100

    grains update

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,20 @@
 def square(number):
-    pass
+    #chessboard is a list of numbers that doubles each time
+    chessboard = []
 
+    #adding the number of grains of wheat in each square
+    x = 1
+    for _ in range(64):
+        chessboard.append(x)
+        x = 2 * x
+    
+    return chessboard
 
 def total():
-    pass
+    #the total of the grains in the chessboard
+    
+    total = 0
+    for i in square(0):
+        total = total + i
+    
+    return total
