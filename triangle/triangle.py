6de9712a2ced772fbc231f14ee1fea1dd6966a49
Abderraouf Idel <abderraoufidel@gmail.com>
2024-10-20 15:36:23 +0100

    triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,19 @@
 def equilateral(sides):
-    pass
+    #first we need to sort the sides a, b, c
+    a, b, c =sorted(sides)
+    #rules: it's a triangle | and a=b=c
+    return a == c > 0 #if the min value of sorted(sides)= the max value of sorted(sides) and > then 0 "a=b=c"
 
 
 def isosceles(sides):
-    pass
+    #sides
+    a, b, c = sorted(sides)
+    #triangle | a + b > c | b = a or c
+    return a + b > c and b in (a,c) and a > 0
 
 
 def scalene(sides):
-    pass
+    #sides
+    a, b, c = sorted(sides)
+    #triangle | a<b<c | a+b>c
+    return 0 < a < b < c < a + b
